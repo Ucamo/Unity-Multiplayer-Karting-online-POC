6f309f24a83aa2478ab73da16106ad45ae7c3110
Uriel Carrillo <carrillouriel@gmail.com>
2021-03-11 22:56:22 -0700

    initial commit, kinda works

---
+++ Assets/MyScripts/LobyController.cs
@@ -0,0 +1,50 @@
+﻿using ExitGames.Client.Photon;
+using Photon.Realtime;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Photon.Pun
+{
+public class LobyController : MonoBehaviourPunCallbacks
+{
+    public InputField PlayerNameInput;
+    public Text lblStatus;
+
+    private Dictionary<string, RoomInfo> cachedRoomList;
+    private Dictionary<string, GameObject> roomListEntries;
+    private Dictionary<int, GameObject> playerListEntries;
+
+    
+    #region UNITY
+    public void Start()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+
+        cachedRoomList = new Dictionary<string, RoomInfo>();
+        roomListEntries = new Dictionary<string, GameObject>();
+            
+        PlayerNameInput.text = "Player " + Random.Range(1000, 10000);
+    }
+
+    #endregion
+
+    public void OnLoginButtonClicked()
+        {
+            string playerName = PlayerNameInput.text;
+
+            if (!playerName.Equals(""))
+            {
+                PhotonNetwork.LocalPlayer.NickName = playerName;
+                PhotonNetwork.ConnectUsingSettings();
+            }
+            else
+            {
+                Debug.LogError("Player Name is invalid.");
+            }
+        }
+
+
+}
+}
+
---
+++ Assets/MyScripts/SetupCamera.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using Photon.Pun.Demo.PunBasics;
+
+public class SetupCamera : MonoBehaviour
+{
+
+    void Start()
+    {
+       //Start the coroutine we define below named SetupCamera.
+        StartCoroutine(WaitToSetupCamera());
+    }
+
+    IEnumerator WaitToSetupCamera()
+    {
+        //yield on a new YieldInstruction that waits for 1 second.
+        yield return new WaitForSeconds(1);
+        SetupCinemachineVirtualCamera();
+    }
+    void SetupCinemachineVirtualCamera(){
+		GameObject cinemachineVC = GameObject.Find("CinemachineVirtualCamera");
+		if(cinemachineVC!=null){
+
+            GameObject gameManager = GameObject.Find("Game Manager");
+            GameObject playerPrefab;
+            if(gameManager!=null){
+                playerPrefab= gameManager.GetComponent<GameManager>().GetPlayerPrefab();
+
+                if(playerPrefab!=null){
+                    cinemachineVC.GetComponent<CinemachineVirtualCamera>().Follow=playerPrefab.transform;
+			        cinemachineVC.GetComponent<CinemachineVirtualCamera>().LookAt=playerPrefab.transform;
+                }else{
+                     Debug.LogError("PlayerPrefab not found");
+                }
+            }else{
+                 Debug.LogError("GameManager not found");
+            }			
+		}else{
+            Debug.LogError("Cinemachine virtual camera not found");
+        }
+	}
+}

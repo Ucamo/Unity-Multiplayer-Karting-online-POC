c6c7a455f558c088d306119426e8243d4559fffd
Uriel Carrillo <carrillouriel@gmail.com>
2021-03-23 01:04:14 -0700

    Pick up a random material for each racer at start

---
+++ Assets/MyScripts/PickRandomMaterial.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ using UnityEngine.UI;
+
+public class PickRandomMaterial : MonoBehaviour
+{
+    public Material[] materialList;
+    void Start()
+    {
+        if(materialList.Length>0){
+            int index = Random.Range(0,materialList.Length-1);
+            gameObject.GetComponent<SkinnedMeshRenderer> ().material = materialList[index];
+        }
+    }
+
+}
